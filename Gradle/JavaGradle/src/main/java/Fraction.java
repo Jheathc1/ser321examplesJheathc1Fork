517be9fe49744e9cef6749626aa6a419c8466178
Jheathc1 <jheathc1@asu.edu>
2024-03-14 12:23:55 -0700

    gradle and fraction changes

--- Gradle/JavaGradle/src/main/java/Fraction.java
+++ Gradle/JavaGradle/src/main/java/Fraction.java
@@ -39,13 +39,23 @@ public class Fraction {
 
    public static void main (String args[]) {
       try {
+	
          // create a new instance
          // Fraction *frac = [[Fraction alloc] init];
          Fraction frac = new Fraction();
 
+
          // set the values
-         frac.setNumerator(1);
-         frac.setDenominator(3);
+	 if (args.length == 0) {
+         	frac.setNumerator(1);
+         	frac.setDenominator(3);
+	 } else if (args.length == 1) {
+         	frac.setNumerator(Integer.parseInt(args[0]));
+	 } else {
+		frac.setNumerator(Integer.parseInt(args[0]));
+         	frac.setDenominator(Integer.parseInt(args[1]));
+	 }
+
 
          // print it
          System.out.print("The fraction is: ");
